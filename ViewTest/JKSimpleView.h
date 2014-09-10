c1feca699bde9dbb1903ee0e626e108c5f301943
Guest User <Guest@plum.cs.hmc.edu>
2014-09-10 16:56:52 -0700

    Did the first part of the tutorial

---
+++ ViewTest/JKSimpleView.h
@@ -0,0 +1,13 @@
+//
+//  JKSimpleView.h
+//  ViewTest
+//
+//  Created by Guest User on 9/10/14.
+//  Copyright (c) 2014 Josh Kutsko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKSimpleView : UIView
+
+@end
---
+++ ViewTest/JKSimpleView.m
@@ -0,0 +1,13 @@
+//
+//  JKSimpleView.m
+//  ViewTest
+//
+//  Created by Guest User on 9/10/14.
+//  Copyright (c) 2014 Josh Kutsko. All rights reserved.
+//
+
+#import "JKSimpleView.h"
+
+@implementation JKSimpleView
+
+@end
